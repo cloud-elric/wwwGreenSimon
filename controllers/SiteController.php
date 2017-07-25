bae72405ccbe8b44be7c812f54222bfa1f0c11ff
cloud-elric <humberto@2gom.com.mx>
2017-07-25 18:34:35 -0500

    Modificacion para el envio de sms

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -122,8 +122,33 @@ class SiteController extends Controller {
 
 	private function sendSMS($tel='', $message=''){
 		
-		$url = 'http://sms-tecnomovil.com/SvtSendSms?username=PIXERED&password=Pakabululu01&message=' . $message . '&numbers=' . $tel;
-		$sms = file_get_contents ( $url );				
+		$urlAutenticate = 'http://sms-tecnomovil.com/SvtSendSms?username=PIXERED&password=Pakabululu01&message=' . $message . '&numbers=' . $tel;
+		//$sms = file_get_contents ( $url );	
+
+		//$urlAutenticate = 'http://dgom.mobi';
+		
+		$ch = curl_init ();
+		
+		curl_setopt ( $ch, CURLOPT_URL, $urlAutenticate );
+		
+		curl_setopt ( $ch, CURLOPT_POSTREDIR, 3 );
+		curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, true );
+		
+		// in real life you should use something like:
+		// curl_setopt($ch, CURLOPT_POSTFIELDS,
+		// http_build_query(array('postvar1' => 'value1')));
+		
+		// receive server response ...
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+		
+		$server_output = curl_exec ( $ch );
+		
+		curl_close ( $ch );
+		
+		echo "<pre>";
+		print_r($server_output);
+		echo "</pre>";
+		exit();			
 
 	}
 
